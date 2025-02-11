60375199e34ea58f51a4129e400382f3583a015b
Dominic Crew <crew.dominic@gmail.com>
2025-02-11 10:00:10 +0000

    Fixed unittest

--- test_weather_extract.py
+++ test_weather_extract.py
@@ -7,7 +7,7 @@ from weather_extract import get_dates
 
 
 class TestGetDates(unittest.TestCase):
-    @patch('datetime.datetime')
+    @patch('weather_extract.datetime')
     def test_get_dates(self, mock_datetime):
         mock_datetime.today.return_value = datetime(2025, 2, 10)
         today, tomorrow = get_dates()
