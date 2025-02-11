266f286004dbaae1837b0a0d32f95f91147dd8b1
Dominic Crew <crew.dominic@gmail.com>
2025-02-11 16:47:55 +0000

    Added test

--- api_exploration/test_weather_extract.py
+++ api_exploration/test_weather_extract.py
@@ -2,8 +2,9 @@
 import unittest
 from datetime import datetime, timedelta
 from unittest.mock import patch
+import pandas as pd
 
-from weather_extract import get_dates
+from weather_extract import get_dates, convert_df_to_list
 
 
 class TestGetDates(unittest.TestCase):
@@ -12,7 +13,15 @@ class TestGetDates(unittest.TestCase):
         mock_datetime.today.return_value = datetime(2025, 2, 10)
         today, tomorrow = get_dates()
         self.assertEqual(today, '2025-02-10')
-        self.assertEqual(tomorrow, '2025-02-11')
+        self.assertEqual(tomorrow, '2025-02-17')
+
+
+class TestConvertDfToList(unittest.TestCase):
+    def test_convert_df_to_list(self):
+        test_df = pd.DataFrame(
+            {'id': [1, 2], 'name': ['Alice', 'Bob']})
+        self.assertEqual(convert_df_to_list(test_df),
+                         [(1, 'Alice'), (2, 'Bob')])
 
 
 if __name__ == "__main__":
