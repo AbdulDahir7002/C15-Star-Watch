a9841f5f7ecfb10f03070a60d70ce7babf2b56a2
Dominic Crew <crew.dominic@gmail.com>
2025-02-11 16:58:37 +0000

    Added more tests

--- api_exploration/test_weather_extract.py
+++ api_exploration/test_weather_extract.py
@@ -1,10 +1,10 @@
 # pylint: skip-file
 import unittest
 from datetime import datetime, timedelta
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 import pandas as pd
 
-from weather_extract import get_dates, convert_df_to_list
+from weather_extract import get_dates, convert_df_to_list, get_locations, clear_weather_table
 
 
 class TestGetDates(unittest.TestCase):
@@ -22,6 +22,40 @@ class TestConvertDfToList(unittest.TestCase):
             {'id': [1, 2], 'name': ['Alice', 'Bob']})
         self.assertEqual(convert_df_to_list(test_df),
                          [(1, 'Alice'), (2, 'Bob')])
+
+
+class TestGetLocations(unittest.TestCase):
+    def test_get_locations(self):
+        mock_conn = MagicMock()
+        mock_cursor = MagicMock()
+
+        mock_conn.cursor.return_value = mock_cursor
+        mock_cursor.fetchall.return_value = [
+            ("test1", "test2"), ("test3", "test4")]
+
+        result = get_locations(mock_conn)
+
+        mock_conn.cursor.assert_called_once()
+        mock_cursor.execute.assert_called_once_with("SELECT * FROM city;")
+        mock_cursor.fetchall.assert_called_once()
+        mock_cursor.close.assert_called_once()
+        self.assertEqual(
+            result, [("test1", "test2"), ("test3", "test4")])
+
+
+class TestClearWeatherTable(unittest.TestCase):
+    def test_clear_weather_table(self):
+        mock_conn = MagicMock()
+        mock_cursor = MagicMock()
+
+        mock_conn.cursor.return_value = mock_cursor
+
+        clear_weather_table(mock_conn)
+        mock_conn.cursor.assert_called_once()
+        mock_cursor.execute.assert_called_once_with(
+            "DELETE FROM weather_status;")
+        mock_conn.commit.assert_called_once()
+        mock_cursor.close.assert_called_once()
 
 
 if __name__ == "__main__":
