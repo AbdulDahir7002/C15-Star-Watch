b780fed9b8f120a5393ff8abe366cc0f75f41ad7
Dominic Crew <crew.dominic@gmail.com>
2025-02-11 14:40:21 +0000

    Seeding.py now adds countries, cities and meteor showers to the DB

--- db_scripts/test_seeding.py
+++ db_scripts/test_seeding.py
@@ -15,7 +15,7 @@ class TestGetCorrectLocation(unittest.TestCase):
                 "longitude": 1, "elevation": 1, "name": "testville", "admin1": "England"},
             {"country": "United States", "latitude": 0, "longitude": 0, "elevation": 0, "name": "test city", "admin1": "America"}]
         self.assertEqual(get_correct_location(
-            test_results), ("testville", 1, 1, 1, 'England'))
+            test_results), ("testville", 1, 1, 1, 1))
 
     def test_get_correct_location_no_uk(self):
         test_results = [{"country": "United States", "latitude": 0,
