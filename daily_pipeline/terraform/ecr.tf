16f5cfc6adf93bacd311b804e2ffb9c2d173dfce
AbdulDahir7002 <abdul.dahir7002@gmail.com>
2025-02-12 17:03:37 +0000

    reorganised folders

--- daily_pipeline/terraform/ecr.tf
+++ daily_pipeline/terraform/ecr.tf
@@ -1,11 +1,11 @@
 provider "aws" {
     access_key = var.AWS_ACCESS_KEY
-    secret_key = var.AWS_SECRET_KEY
+    secret_key = var.AWS_SECRET_ACCESS_KEY
     region = var.AWS_REGION
 }
 
 resource "awscc_ecr_repository" "c15-star-watch-lambdas-repo" {
-  repository_name      = "c15-star-watch-lambdas-repo"
+  repository_name      = "c15-star-watch-daily-lambda-repo"
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration = {
     scan_on_push = true
