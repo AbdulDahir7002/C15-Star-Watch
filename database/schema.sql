4a417385ad392e7bf9c6763a47392fed6e99933e
Beenzu <beenzu003@gmail.com>
2025-02-12 15:31:55 +0000

    stricter constraints

--- database/schema.sql
+++ database/schema.sql
@@ -14,12 +14,12 @@ CREATE TABLE meteor_shower (
 
 CREATE TABLE moon_phase (
     moon_phase_id SMALLINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    moon_phase_name VARCHAR(50) NOT NULL
+    moon_phase_name VARCHAR(50) UNIQUE NOT NULL
 );
 
 CREATE TABLE country (
     country_id SMALLINT PRIMARY KEY NOT NULL,
-    country_name VARCHAR(50) NOT NULL
+    country_name VARCHAR(50) UNIQUE NOT NULL
 );
 
 CREATE TABLE aurora_status (
@@ -28,7 +28,8 @@ CREATE TABLE aurora_status (
     camera_visibility BOOLEAN NOT NULL,
     naked_eye_visibility BOOLEAN NOT NULL,
     country_id SMALLINT NOT NULL,
-    FOREIGN KEY (country_id) REFERENCES country(country_id)
+    FOREIGN KEY (country_id) REFERENCES country(country_id),
+    UNIQUE (aurora_status_at, camera_visibility, naked_eye_visibility, country_id)
 );
 
 CREATE TABLE city (
@@ -48,7 +49,8 @@ CREATE TABLE weather_status (
     coverage FLOAT NOT NULL,
     visibility FLOAT NOT NULL,
     status_at TIMESTAMP NOT NULL,
-    FOREIGN KEY (city_id) REFERENCES city(city_id)
+    FOREIGN KEY (city_id) REFERENCES city(city_id),
+    UNIQUE (city_id, status_at)
 );
 
 CREATE TABLE stargazing_status (
@@ -62,7 +64,8 @@ CREATE TABLE stargazing_status (
     moon_phase_url VARCHAR(100) NOT NULL,
     nasa_apod_url VARCHAR(100) NOT NULL,
     FOREIGN KEY (city_id) REFERENCES city(city_id),
-    FOREIGN KEY (moon_phase_id) REFERENCES moon_phase(moon_phase_id)
+    FOREIGN KEY (moon_phase_id) REFERENCES moon_phase(moon_phase_id),
+    UNIQUE(city_id, moon_phase_id, sunrise, sunset, status_date, star_chart_url, moon_phase_url, nasa_apod_url)
 );
 
 CREATE TABLE meteor_shower_assignment (
