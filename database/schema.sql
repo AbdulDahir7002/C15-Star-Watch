df5446bc97ad7aadf28b5e0b6630fe5bee9f0ca7
Beenzu <beenzu003@gmail.com>
2025-02-12 10:23:18 +0000

    updated schema

--- database/schema.sql
+++ database/schema.sql
@@ -16,15 +16,18 @@ CREATE TABLE moon_phase (
     moon_phase_name VARCHAR NOT NULL
 );
 
-CREATE TABLE aurora_status (
-    aurora_status_id SMALLINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    aurora_status_colour VARCHAR NOT NULL,
-    aurora_status_desc TEXT NOT NULL
-);
-
 CREATE TABLE country (
     country_id SMALLINT PRIMARY KEY NOT NULL,
     country_name VARCHAR NOT NULL
+);
+
+CREATE TABLE aurora_status (
+    aurora_status_id SMALLINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    aurora_hourly_status_at TIMESTAMP NOT NULL,
+    camera_visibility BOOLEAN NOT NULL,
+    naked_eye_visibility BOOLEAN NOT NULL,
+    country_id SMALLINT NOT NULL,
+    FOREIGN KEY (country_id) REFERENCES country(country_id)
 );
 
 CREATE TABLE city (
@@ -49,10 +52,15 @@ CREATE TABLE weather_status (
 
 CREATE TABLE subscriber (
     subscriber_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    subscriber_name VARCHAR NOT NULL,
     subscriber_email VARCHAR,
-    subscriber_phone VARCHAR,
+    subscriber_phone VARCHAR
+);
+
+CREATE TABLE subscriber_city_assignment (
+    subscriber_city_assignment_id SMALLINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    subscriber_id INT NOT NULL,
     city_id SMALLINT NOT NULL,
+    FOREIGN KEY (subscriber_id) REFERENCES subscriber(subscriber_id),
     FOREIGN KEY (city_id) REFERENCES city(city_id)
 );
 
@@ -60,16 +68,20 @@ CREATE TABLE stargazing_status (
     stargazing_status_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     city_id SMALLINT NOT NULL,
     moon_phase_id SMALLINT NOT NULL,
-    aurora_status_id SMALLINT NOT NULL,
     sunrise TIMESTAMP NOT NULL,
     sunset TIMESTAMP NOT NULL,
     status_date DATE NOT NULL,
     star_chart_url VARCHAR NOT NULL,
-    meteor_shower_id SMALLINT,
     moon_phase_url VARCHAR,
     nasa_apod_url VARCHAR,
     FOREIGN KEY (city_id) REFERENCES city(city_id),
-    FOREIGN KEY (moon_phase_id) REFERENCES moon_phase(moon_phase_id),
-    FOREIGN KEY (aurora_status_id) REFERENCES aurora_status(aurora_status_id),
-    FOREIGN KEY (meteor_shower_id) REFERENCES meteor_shower(meteor_shower_id)
+    FOREIGN KEY (moon_phase_id) REFERENCES moon_phase(moon_phase_id)
+);
+
+CREATE TABLE meteor_shower_assignment (
+    meteor_shower_assignment_id SMALLINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    meteor_shower_id SMALLINT NOT NULL,
+    stargazing_status_id SMALLINT NOT NULL,
+    FOREIGN KEY (meteor_shower_id) REFERENCES meteor_shower(meteor_shower_id),
+    FOREIGN KEY (stargazing_status_id) REFERENCES stargazing_status(stargazing_status_id)
 );
