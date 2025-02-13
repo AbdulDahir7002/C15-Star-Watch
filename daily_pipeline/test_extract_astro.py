8966d4bf741247a14788156cd02300ca75937ad1
tesspiastra <tpiastra246@gmail.com>
2025-02-13 09:09:22 +0000

    adding test file and updated requirements

---
+++ daily_pipeline/test_extract_astro.py
@@ -0,0 +1,38 @@
+# pylint: skip-file
+import pytest
+from dotenv import load_dotenv
+
+from extract_astro import get_connection, get_locations, post_location_get_moonphase, post_location_get_starchart, collate_data
+from unittest.mock import patch, MagicMock
+
+load_dotenv()
+
+
+@patch("extract_astro.psycopg2.connect", return_value="mocked_conn")
+def test_connection_made(mock_conn):
+    """Tests that the connection function is called once"""
+    get_connection()
+    assert mock_conn.call_count == 1
+
+
+def test_city_format():
+    """Tests that the query has the right format"""
+    pass
+
+
+@patch("extract_astro.psycopg2.connect")
+def test_cursor_closes(mock_connect):
+    """Tests the cursor is closed"""
+    mock_conn = MagicMock()
+    mock_cursor = MagicMock()
+
+    # Ensure the connection returns the mock cursor when calling cursor()
+    mock_conn.cursor.return_value = mock_cursor
+    # Mock connection returned by psycopg2.connect
+    mock_connect.return_value = mock_conn
+
+    # Call the function under test
+    get_locations(mock_conn)
+
+    # Assertions
+    mock_cursor.close.assert_called_once()
