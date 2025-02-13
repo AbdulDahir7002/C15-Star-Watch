eb17abe797d7ad88abb3ae93625226b1f88238ab
Beenzu <beenzu003@gmail.com>
2025-02-13 11:50:18 +0000

    undatablock

--- hourly_pipeline/terraform/hourly_scheduler.tf
+++ hourly_pipeline/terraform/hourly_scheduler.tf
@@ -1,9 +1,32 @@
-data "aws_iam_role" "scheduler_role" {
+resource "aws_iam_role" "scheduler_role" {
     name = "EventBridgeSchedulerRole"
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action = "sts:AssumeRole"
+                Effect = "Allow"
+                Principal = {
+                    Service = "scheduler.amazonaws.com"
+                }
+            }
+        ]
+    })
 }
 
-data "aws_iam_role_policy" "scheduler_lambda_invoke" {
+resource "aws_iam_role_policy" "scheduler_lambda_invoke" {
     name = "EventBridgeInvokeLambdaPolicy"
+    role = aws_iam_role.scheduler_role.id
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+            Effect = "Allow"
+            Action = "lambda:InvokeFunction"
+            Resource = aws_lambda_function.pipeline-lambda.arn
+        }
+        ]
+    })
 }
 
 
@@ -19,6 +42,6 @@ resource "aws_scheduler_schedule" "hourly_scheduler" {
 
   target {
     arn      = aws_lambda_function.pipeline-lambda.arn # possibly may need to change since both etl lambdas are called "pipeline-lambda"
-    role_arn = data.aws_iam_role.scheduler_role.arn
+    role_arn = aws_iam_role.scheduler_role.arn
   }
 }
