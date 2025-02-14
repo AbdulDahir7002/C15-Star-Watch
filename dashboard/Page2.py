ada32c97eeab24c332aa428f114ca43ad5d4541c
AbdulDahir7002 <abdul.dahir7002@gmail.com>
2025-02-14 16:54:27 +0000

    added meteor timeline

--- dashboard/Page2.py
+++ dashboard/Page2.py
@@ -7,6 +7,7 @@ import psycopg2.extras
 import streamlit as st
 import pandas as pd
 import psycopg2
+from streamlit_timeline import st_timeline
 
 # DB connect
 
@@ -24,8 +25,8 @@ def get_connection():
     return connection
 
 
-def query_db(conn, query: str, params: tuple) -> list[tuple]:
-    """Query the database and return a list of tuples of values"""
+def query_db(conn, query: str, params: tuple) -> list[dict]:
+    """Query the database and return a list of tuples of values."""
     print(conn)
     with conn.cursor() as cursor:
         cursor.execute(query, params)
@@ -33,7 +34,8 @@ def query_db(conn, query: str, params: tuple) -> list[tuple]:
     return output
 
 
-def get_sunrise_sunset_pandas(conn):
+def get_sunrise_sunset_df(conn):
+    """Returns a dataframe containing sunrise and sunset data."""
     q = """
             SELECT 
                 s.stargazing_status_id, 
@@ -46,18 +48,80 @@ def get_sunrise_sunset_pandas(conn):
     return pd.DataFrame(rows)
 
 
+def get_weather_status_week_df(conn):
+    """Returns a dataframe containing weather status data."""
+    q = """
+        SELECT 
+            w.weather_status_id, 
+            c.city_name, 
+            w.temperature, 
+            w.coverage, 
+            w.visibility,
+            w.status_at
+        FROM
+            weather_status AS w
+        JOIN
+            city as c USING (city_id)
+        WHERE
+            w.status_at <= CURRENT_DATE + INTERVAL '2 days'; 
+        """
+    rows = query_db(conn, q, [])
+    return pd.DataFrame(rows)
+
+
+def get_meteor_shower_df(conn):
+    """Returns a dataframe containing meteor shower data."""
+    q = """
+        SELECT 
+            meteor_shower_id,
+            meteor_shower_name,
+            shower_start,
+            shower_end,
+            shower_peak 
+        FROM meteor_shower
+        """
+    rows = query_db(conn, q, [])
+    return rows
+
+
+def meteor_timeline(meteor_df):
+
+    items = []
+    for item in meteor_df:
+        items.append({
+            "id": int(item["meteor_shower_id"]),
+            "content": item["meteor_shower_name"],
+            "start": str(item["shower_start"]),
+            "end": str(item["shower_end"])
+        })
+
+    timeline = st_timeline(items=items,
+                           groups=[], options={}, height="300px")
+    st.subheader("Selected Meteor Shower")
+    st.markdown(f"*{timeline["content"].capitalize()}*")
+
+
 def app():
-    """Layout of page 2 of the dashboard"""
+    """Layout of page 2 of the dashboard."""
     load_dotenv()
     connection = get_connection()
 
-    st.title("Page 2")
+    st.title("Weekly trends")
     st.write("This is Page 2.")
     st.write("You can add more content here!")
 
-    sunrise_sunset_df = get_sunrise_sunset_pandas(connection)
-
+    sunrise_sunset_df = get_sunrise_sunset_df(connection)
     st.write(sunrise_sunset_df)
+
+    weather_status_df = get_weather_status_week_df(connection)
+    st.write(weather_status_df)
+
+    meteor_shower_dict = get_meteor_shower_df(connection)
+    meteor_shower_df = pd.DataFrame(meteor_shower_dict)
+    st.write(meteor_shower_df)
+
+    st.write("Meteor shower timeline")
+    meteor_timeline(meteor_shower_dict)
 
 
 if __name__ == "__main__":
