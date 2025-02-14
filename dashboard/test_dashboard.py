d9cfd6d6f2a42f847d1d10e0ba0b315ad6b94da7
Dominic Crew <crew.dominic@gmail.com>
2025-02-14 09:52:34 +0000

    Added test for get_aurora_info

--- dashboard/test_dashboard.py
+++ dashboard/test_dashboard.py
@@ -1,11 +1,11 @@
 # pylint: skip-file
 import unittest
 from unittest.mock import patch, MagicMock
-from datetime import date
+from datetime import date, datetime
 
 import pandas as pd
 
-from Page1 import get_weather_for_day
+from Page1 import get_weather_for_day, get_aurora_info
 
 
 class TestGetWeatherForDay(unittest.TestCase):
@@ -19,14 +19,32 @@ class TestGetWeatherForDay(unittest.TestCase):
 
         result = get_weather_for_day(date(2025, 2, 14), 'TestCity', mock_conn)
 
-        expected_result = pd.DataFrame([[6], [14.7],
+        expected_result = pd.DataFrame([['06'], [14.7],
                                         ['86'], ['12345']],
                                        index=['Time', 'Temperature',
                                               'Coverage', 'Visibility'],
                                        columns=[0])
         pd.testing.assert_frame_equal(result, expected_result)
-        self.assertTrue(mock_cursor.execute.called)
-        self.assertTrue(mock_cursor.fetchall.called)
+        mock_conn.cursor.assert_called_once()
+        mock_cursor.close.assert_called_once()
+
+
+class TestGetAuroraInfo(unittest.TestCase):
+    def test_get_aurora_info(self):
+        mock_conn = MagicMock()
+        mock_cursor = MagicMock()
+
+        mock_conn.cursor.return_value = mock_cursor
+        mock_cursor.fetchall.return_value = [(0,
+                                              datetime(2025, 2, 14, 8, 57, 32), False, False, 2)]
+        result = get_aurora_info(2, mock_conn)
+        expected_result = pd.DataFrame(
+            {'Recording At': ['2025-02-14 08:57:32'],
+             'Visible by Camera': ['False'],
+             'Visible by Eye': ['False']})
+        pd.testing.assert_frame_equal(result, expected_result)
+        mock_conn.cursor.assert_called_once()
+        mock_cursor.close.assert_called_once()
 
 
 if __name__ == "__main__":
