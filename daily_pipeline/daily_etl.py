aa20782c64523a4d426e5d94e3b4677ed76fa8e8
tesspiastra <tpiastra246@gmail.com>
2025-02-17 17:46:29 +0000

    choice of logging method and untracking logs

--- daily_pipeline/daily_etl.py
+++ daily_pipeline/daily_etl.py
@@ -19,8 +19,8 @@ def configure_logs():
         style="{",
         datefmt="%Y-%m-%d %H:%M",
         handlers=[
-            logging.FileHandler("logs/pipeline.log", mode="a",
-                                encoding="utf-8"),
+            # logging.FileHandler("logs/pipeline.log", mode="a",
+            #                     encoding="utf-8"),
             logging.StreamHandler(sys.stdout)
         ]
     )
