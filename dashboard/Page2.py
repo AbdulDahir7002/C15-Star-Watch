c8da2e4f02ee7a5b35afd7070a4eac7346be91b7
AbdulDahir7002 <abdul.dahir7002@gmail.com>
2025-02-17 18:07:25 +0000

    page 2 changes

--- dashboard/Page2.py
+++ dashboard/Page2.py
@@ -46,14 +46,15 @@ def get_avg_sunrise_sunset_df(conn):
             FROM stargazing_status AS s
             JOIN city as c USING (city_id)
             GROUP BY
-                s.status_date;
+                s.status_date
+            ORDER BY
+                s.status_date
+            ;
             """
     rows = query_db(conn, q, [])
     if len(rows) == 0:
         return pd.DataFrame(columns=["sunrise", "sunset", "status_date"])
     sunrise_sunset_df = pd.DataFrame(rows)
-    sunrise_sunset_df["sunrise"] = sunrise_sunset_df["sunrise"].dt.time
-    sunrise_sunset_df["sunset"] = sunrise_sunset_df["sunset"].dt.time
     return sunrise_sunset_df
 
 
@@ -99,7 +100,18 @@ def get_meteor_shower_data(conn) -> list[dict]:
     return rows
 
 
+def set_aurora_vis_label(value):
+    """Apply relevant aurora status depending on value"""
+    if value == 0:
+        return "Not visible"
+    elif value == 1:
+        return "Visible to cameras"
+    else:
+        return "Fully visible"
+
+
 def get_aurora_status_df(conn):
+    """Gets aurora data and transforms it"""
     q = """
         SELECT 
             a.aurora_status_id,
@@ -119,12 +131,15 @@ def get_aurora_status_df(conn):
     aurora_df = pd.DataFrame(rows)
     aurora_df["aurora_vis"] = sum([aurora_df["camera_visibility"],
                                    aurora_df["naked_eye_visibility"]])
+    aurora_df["aurora_vis_label"] = aurora_df["aurora_vis"].apply(
+        set_aurora_vis_label)
     return aurora_df
 
 # Visualisations
 
 
 def meteor_timeline(meteor_df: pd.DataFrame):
+    """Displays a timeline of currently known meteor showers"""
     items = []
     for item in meteor_df:
         items.append({
@@ -141,25 +156,53 @@ def meteor_timeline(meteor_df: pd.DataFrame):
         st.write(f"*Select a meteor shower for details*")
     else:
         st.markdown(f"*{timeline["content"].capitalize()}*")
-        st.write(f"Date range: {timeline["start"]} - {timeline["end"]}")
+        st.write(
+            f"From :orange[*{timeline["start"]}*] to :orange[*{timeline["end"]}*]")
         # TODO: return more details
 
 
 def sunrise_sunset_line(sunrise_sunset_dataframe: pd.DataFrame):
-    ss_line = alt.Chart(sunrise_sunset_dataframe).mark_line().encode(
-        alt.X("status_date"),
-        alt.Y("sunrise"))
-    st.altair_chart(ss_line)
+    """Graphs sunrise and sunset over time"""
+    sunrise_line = alt.Chart(sunrise_sunset_dataframe).mark_line().encode(
+        alt.X("monthdate(status_date):T"),
+        alt.Y("hoursminutes(sunrise):T"))
+    sunset_line = alt.Chart(sunrise_sunset_dataframe).mark_line().encode(
+        alt.X("monthdate(status_date):T"),
+        alt.Y("hoursminutes(sunset):T"))
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.altair_chart(sunrise_line)
+    with col2:
+        st.altair_chart(sunset_line)
     # TODO: Fix the output by making the y-axis time (not datetime or epoch)
 
 
 def aurora_status_timeline(aurora_df: pd.DataFrame):
+    """Maps aurora visibility over time"""
     aurora_line = alt.Chart(aurora_df).mark_line().encode(
         alt.X("aurora_status_at"),
-        alt.Y("aurora_vis"),
+        alt.Y("aurora_vis_label"),
         color="country_name"
     )
     st.altair_chart(aurora_line)
+
+
+def aurora_status_bar_charts(aurora_df: pd.DataFrame):
+    """Bar chart comparing aurora visibility for each country"""
+    aurora_df = aurora_df[aurora_df["aurora_vis"] > 0]
+    aurora_cam_vis = aurora_df[[
+        "country_name", "naked_eye_visibility"]].value_counts().reset_index()
+    aurora_cam_vis["naked_eye_visibility"] = aurora_cam_vis["naked_eye_visibility"].map(
+        {True: "Camera Visible", False: "Fully Visible"})
+    st.write(aurora_cam_vis)
+
+    aurora_bar = alt.Chart(aurora_cam_vis).mark_bar().encode(
+        alt.X("naked_eye_visibility").title("Visibility"),
+        alt.Y("count").title("Count"),
+        color="country_name")
+    st.altair_chart(aurora_bar)
 
 
 def app():
@@ -168,29 +211,22 @@ def app():
     connection = get_connection()
 
     st.title("Weekly trends")
-    st.write("This is Page 2.")
-    st.write("You can add more content here!")
 
+    st.markdown("## Sunrise and Sunset average over time &#9728;")
     avg_sunrise_sunset_df = get_avg_sunrise_sunset_df(connection)
-    # st.write(avg_sunrise_sunset_df)
-
     sunrise_sunset_line(avg_sunrise_sunset_df)
 
     weather_status_df = get_weather_status_week_df(connection)
-    st.write(weather_status_df)
 
+    st.markdown("## Meteor shower timeline &#x2604;")
     meteor_shower_dict = get_meteor_shower_data(connection)
     meteor_shower_df = pd.DataFrame(meteor_shower_dict)
-    # st.write(meteor_shower_df)
-
-    st.markdown("## Meteor shower timeline")
     meteor_timeline(meteor_shower_dict)
 
-    st.write("Aurora Data")
+    st.markdown("## Aurora Data &#10024;")
     aurora_dataframe = get_aurora_status_df(connection)
-    # st.write(aurora_dataframe)
-
     aurora_status_timeline(aurora_dataframe)
+    aurora_status_bar_charts(aurora_dataframe)
 
 
 if __name__ == "__main__":
