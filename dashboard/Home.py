d179575c683c589cde008208e9501053afc47fd1
Dominic Crew <crew.dominic@gmail.com>
2025-02-18 10:52:46 +0000

    Added NASA APOD to home page

--- dashboard/Home.py
+++ dashboard/Home.py
@@ -1,11 +1,39 @@
 """Streamlit dashboard home page."""
 from os import environ as ENV
+import requests
 
 from datetime import date
 from dotenv import load_dotenv
 import streamlit as st
 
 from Page1 import get_constellations, get_constellation_code, get_lat_and_long, post_location_get_starchart
+
+
+@st.cache_data
+def get_nasa_apod() -> dict:
+    """Returns the data for the NASA APOD."""
+    API_KEY = ENV['NASA_APOD_KEY']
+    url = f"https://api.nasa.gov/planetary/apod?api_key={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+
+    print(f"Title: {data['title']}")
+    print(f"Date: {data['date']}")
+    print(f"Explanation: {data['explanation']}")
+    print(f"Image URL: {data['url']}")
+
+    return data
+
+
+def display_apod(apod: dict) -> None:
+    """Displays APOD on streamlit page."""
+    st.title("NASA APOD")
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image(apod['url'])
+    with col2:
+        st.write(apod['title'], '-', apod['date'])
+        st.write(apod['explanation'])
 
 
 def app():
@@ -15,6 +43,7 @@ def app():
     st.title("Starwatch")
     st.write("Welcome to starwatch!")
     st.write("Select a page from the sidebar to explore more.")
+    display_apod(get_nasa_apod())
     constellation = st.selectbox('Constellation', get_constellations())
     code = get_constellation_code(constellation)
     city = 'Aberdeen'
