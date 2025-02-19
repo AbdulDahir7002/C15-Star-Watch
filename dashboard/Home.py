458b702e3911ddff0ad454d397705b21ea431ac6
Dominic Crew <crew.dominic@gmail.com>
2025-02-19 11:14:02 +0000

    Added containers to the home page

--- dashboard/Home.py
+++ dashboard/Home.py
@@ -21,7 +21,7 @@ def get_nasa_apod() -> dict:
 
 def display_apod(apod: dict) -> None:
     """Displays APOD on streamlit page."""
-    st.title("NASA APOD")
+    st.markdown("<h3>NASA Picture of the Day</h3>", unsafe_allow_html=True)
     col1, col2 = st.columns(2)
     with col1:
         st.image(apod['url'])
@@ -30,18 +30,11 @@ def display_apod(apod: dict) -> None:
         st.write(apod['explanation'])
 
 
-def app():
-    """Function that is ran when the user selects the home page."""
-    load_dotenv()
+def display_constellation() -> None:
+    """Displays the constellation starchart section."""
     HEADER = f'Basic {ENV["ASTRONOMY_BASIC_AUTH_KEY"]}'
-
-    st.title("Starwatch")
-    st.write("Welcome to Starwatch!")
-    st.write("Select a page from the sidebar to explore cities in the UK, or stay here for more general information.")
-
-    display_apod(get_nasa_apod())
-
-    constellation = st.selectbox('Constellation', get_constellations())
+    st.markdown("<h3>Constellation Starchart</h3>", unsafe_allow_html=True)
+    constellation = st.selectbox('Select constellation:', get_constellations())
     code = get_constellation_code(constellation)
     city = 'Aberdeen'
     lat, long = get_lat_and_long(city)
@@ -49,6 +42,21 @@ def app():
     url = post_location_get_starchart(
         HEADER, lat, long, day, code)
     create_scroll_image(url, 617, 800)
+
+
+def app():
+    """Function that is ran when the user selects the home page."""
+    load_dotenv()
+
+    st.title("Starwatch")
+    st.write("Welcome to Starwatch!")
+    st.write("Select a page from the sidebar to explore cities in the UK, or stay here for more general information.")
+
+    with st.container(border=True):
+        display_apod(get_nasa_apod())
+
+    with st.container(border=True):
+        display_constellation()
 
 
 if __name__ == "__main__":
