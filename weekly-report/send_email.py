bb4cd4c6a05be49050d979b300e01c4a76b8f1ac
Beenzu <beenzu003@gmail.com>
2025-02-19 17:27:35 +0000

    tests

--- weekly-report/send_email.py
+++ weekly-report/send_email.py
@@ -27,8 +27,7 @@ def list_all_topics(sns: client):
 
 def list_relevant_topics(all_topics: list) -> list:
     """Returns a list of starwatch topics on AWS."""
-    topics = all_topics
-    starwatch_topics = [name for name in topics if re.search(
+    starwatch_topics = [name for name in all_topics if re.search(
         "(c15-star-watch-)(.*)", name)]
     return starwatch_topics
 
