c2dc3141e3532b4e3f0aba9d2c4358f58fda3d6b
AbdulDahir7002 <abdul.dahir7002@gmail.com>
2025-02-20 15:34:52 +0000

    merge deployment scripts

--- daily_pipeline/push_image.sh
+++ daily_pipeline/push_image.sh
@@ -1,7 +1,9 @@
+cd daily_pipeline 
+source .env
 aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 129033205317.dkr.ecr.eu-west-2.amazonaws.com
-
-docker build --platform linux/arm64 --provenance=false -t c15-star-watch-daily-lambda-repo .
-
-docker tag c15-star-watch-daily-lambda-repo:latest 129033205317.dkr.ecr.eu-west-2.amazonaws.com/c15-star-watch-daily-lambda-repo:latest
-
-docker push 129033205317.dkr.ecr.eu-west-2.amazonaws.com/c15-star-watch-daily-lambda-repo:latest 
+docker build --platform linux/amd64 --provenance=false -t $DAILY_ECR_NAME .
+docker tag $DAILY_ECR_NAME:latest 129033205317.dkr.ecr.eu-west-2.amazonaws.com/$DAILY_ECR_NAME:latest
+docker push 129033205317.dkr.ecr.eu-west-2.amazonaws.com/$DAILY_ECR_NAME:latest 
+aws lambda update-function-code \
+           --function-name $DAILY_LAMBDA_NAME \
+           --image-uri 129033205317.dkr.ecr.eu-west-2.amazonaws.com/$DAILY_ECR_NAME:latest
--- dashboard/ec2_deploy.sh
+++ dashboard/ec2_deploy.sh
@@ -1,11 +1,12 @@
-scp -i .pemkey requirements.txt ec2-user@35.176.192.172:requirements.txt
-scp -i .pemkey .env ec2-user@35.176.192.172:.env
+scp -i .pemkey requirements.txt ec2-user@35.176.192.172:dashboard/requirements.txt
+scp -i .pemkey .env ec2-user@35.176.192.172:dashboard/.env
 
 
-scp -i .pemkey Home.py ec2-user@35.176.192.172:Home.py
-scp -i .pemkey main.py ec2-user@35.176.192.172:main.py
-scp -i .pemkey Page1.py ec2-user@35.176.192.172:Page1.py
-scp -i .pemkey Page2.py ec2-user@35.176.192.172:Page2.py
-scp -i .pemkey subscribe.py ec2-user@35.176.192.172:subscribe.py
-scp -i .pemkey Subscriber.py ec2-user@35.176.192.172:Subscriber.py
+scp -i .pemkey Home.py ec2-user@35.176.192.172:dashboard/Home.py
+scp -i .pemkey main.py ec2-user@35.176.192.172:dashboard/main.py
+scp -i .pemkey Page1.py ec2-user@35.176.192.172:dashboard/Page1.py
+scp -i .pemkey Page2.py ec2-user@35.176.192.172:dashboard/Page2.py
+scp -i .pemkey subscribe.py ec2-user@35.176.192.172:dashboard/subscribe.py
+scp -i .pemkey Subscriber.py ec2-user@35.176.192.172:dashboard/Subscriber.py
+scp -i .pemkey Unsubscribe.py ec2-user@35.176.192.172:dashboard/Unsubscribe.py
 
