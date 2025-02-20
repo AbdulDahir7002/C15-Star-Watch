bb31f8401a9a72c506505f6621c1491e929e8fae
AbdulDahir7002 <abdul.dahir7002@gmail.com>
2025-02-20 15:40:55 +0000

    push image scripts

--- weekly-report/push_image.sh
+++ weekly-report/push_image.sh
@@ -1,7 +1,9 @@
+cd weekly-report
+source .env
 aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 129033205317.dkr.ecr.eu-west-2.amazonaws.com
-
-docker build --platform linux/arm64 --provenance=false -t c15-star-watch-email-report-lambda-repo .
-
-docker tag c15-star-watch-email-report-lambda-repo:latest 129033205317.dkr.ecr.eu-west-2.amazonaws.com/c15-star-watch-email-report-lambda-repo:latest
-
-docker push 129033205317.dkr.ecr.eu-west-2.amazonaws.com/c15-star-watch-email-report-lambda-repo:latest
+docker build --platform linux/amd64 --provenance=false -t "$WEEKLY_ECR_NAME" .
+docker tag $WEEKLY_ECR_NAME:latest 129033205317.dkr.ecr.eu-west-2.amazonaws.com/$WEEKLY_ECR_NAME:latest
+docker push 129033205317.dkr.ecr.eu-west-2.amazonaws.com/$WEEKLY_ECR_NAME:latest 
+aws lambda update-function-code \
+           --function-name $WEEKLY_LAMBDA_NAME \
+           --image-uri 129033205317.dkr.ecr.eu-west-2.amazonaws.com/$WEEKLY_ECR_NAME:latest
