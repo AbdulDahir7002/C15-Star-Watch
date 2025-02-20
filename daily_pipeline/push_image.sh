f699dc7ebc12460241d6ced04454bbc722cbf6ef
AbdulDahir7002 <abdul.dahir7002@gmail.com>
2025-02-20 11:22:11 +0000

    architecture change lambda script

--- daily_pipeline/push_image.sh
+++ daily_pipeline/push_image.sh
@@ -1,6 +1,6 @@
 aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 129033205317.dkr.ecr.eu-west-2.amazonaws.com
 
-docker build --platform linux/amd64 --provenance=false -t c15-star-watch-daily-lambda-repo .
+docker build --platform linux/arm64 --provenance=false -t c15-star-watch-daily-lambda-repo .
 
 docker tag c15-star-watch-daily-lambda-repo:latest 129033205317.dkr.ecr.eu-west-2.amazonaws.com/c15-star-watch-daily-lambda-repo:latest
 
