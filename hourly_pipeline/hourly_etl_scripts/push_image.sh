1d4a3b1d8a6b2d9cabc5ec54603f22e73bca68c1
AbdulDahir7002 <abdul.dahir7002@gmail.com>
2025-02-21 11:38:15 +0000

    cd into directory beforehand

--- hourly_pipeline/hourly_etl_scripts/push_image.sh
+++ hourly_pipeline/hourly_etl_scripts/push_image.sh
@@ -1,4 +1,3 @@
-cd hourly_pipeline/hourly_etl_script
 source .env
 aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 129033205317.dkr.ecr.eu-west-2.amazonaws.com
 docker build --platform linux/amd64 --provenance=false -t $HOURLY_ECR_NAME .
